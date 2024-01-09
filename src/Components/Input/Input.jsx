dbc09670a340e798b6383bc8fbb12c4622ed2388
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2024-01-09 22:10:14 +0530

    created input component
    this does not handle the legend still

---
+++ src/Components/Input/Input.jsx
@@ -0,0 +1,51 @@
+import { Fragment } from "react";
+import PropTypes from "prop-types";
+
+function Input({ inputData }) {
+  const { legend } = inputData;
+  return (
+    <Fragment>
+      {!legend ? (
+        <div className="inputs">
+          <label htmlFor={inputData["labelFor"]}>
+            {inputData["labelText"]} <i>{inputData["labelUnit"]}</i>{" "}
+            {inputData["labelSymbol"]}
+            <sub>{inputData["labelSymbolSub"]}</sub>
+            &emsp;
+          </label>
+          <input
+            type={inputData["inputType"]}
+            name={inputData["inputName"]}
+            id={inputData["inputId"]}
+            pattern={inputData["inputPattern"]}
+            size={inputData["inputSize"]}
+            placeholder={inputData["inputPlaceholder"]}
+            required={inputData["inputRequired"]}
+          ></input>
+        </div>
+      ) : (
+        "do nothing"
+      )}
+    </Fragment>
+  );
+}
+
+export default Input;
+
+Input.propTypes = {
+  inputData: PropTypes.shape({
+    legend: PropTypes.object,
+    labelFor: PropTypes.string.isRequired,
+    labelText: PropTypes.string.isRequired,
+    labelUnit: PropTypes.string.isRequired,
+    labelSymbol: PropTypes.string.isRequired,
+    labelSymbolSub: PropTypes.string.isRequired,
+    inputType: PropTypes.string.isRequired,
+    inputName: PropTypes.string.isRequired,
+    inputId: PropTypes.string.isRequired,
+    inputPattern: PropTypes.string.isRequired,
+    inputSize: PropTypes.string.isRequired,
+    inputPlaceholder: PropTypes.string.isRequired,
+    inputRequired: PropTypes.boolean,
+  }),
+};
