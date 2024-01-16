39326ecdb30eca09558454b97e509688aae594a5
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2024-01-16 22:36:41 +0530

    added the fieldset things

--- src/Components/Fieldset/Fieldset.jsx
+++ src/Components/Fieldset/Fieldset.jsx
@@ -1,5 +1,33 @@
-function Fieldset() {
-  return <fieldset></fieldset>;
+import PropTypes from "prop-types";
+
+import Legend from "../Legend/Legend";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+
+function Fieldset({ fieldset }) {
+  const { legend, inputs, fieldsets, button } = fieldset;
+  return (
+    <fieldset>
+      <Legend legendData={legend} />
+      {inputs.map((input) => (
+        <Input key={input.labelFor + input.labelText} inputData={input} />
+      ))}
+      {fieldsets &&
+        fieldsets.map((fieldset) => (
+          <Fieldset key={fieldset["legend"]["text"]} fieldset={fieldset} />
+        ))}
+      {button && <Button buttonData={button} />}
+    </fieldset>
+  );
 }
 
 export default Fieldset;
+
+Fieldset.propTypes = {
+  fieldset: PropTypes.shape({
+    legend: PropTypes.object.isRequired,
+    inputs: PropTypes.array.isRequired,
+    fieldsets: PropTypes.array,
+    button: PropTypes.object,
+  }),
+};
