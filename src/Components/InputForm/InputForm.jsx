3f3eeb3c62ac33d3fc98e8ab26524207d9b85a60
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2024-01-16 22:37:06 +0530

    added the form action thing but need to refine it

--- src/Components/InputForm/InputForm.jsx
+++ src/Components/InputForm/InputForm.jsx
@@ -1,13 +1,18 @@
 import PropTypes from "prop-types";
-import { Form } from "react-router-dom";
+import { Form, useActionData } from "react-router-dom";
 import Fieldset from "../Fieldset/Fieldset";
+import { Fragment } from "react";
 
 function InputForm({ formData }) {
+  const actionData = useActionData();
   return (
-    <Form>
-      {console.log(formData)}
-      <Fieldset fieldset={formData} />
-    </Form>
+    <Fragment>
+      <Form method="POST">
+        <Fieldset fieldset={formData} />
+      </Form>
+      {actionData && actionData.error && <p>{actionData.error}</p>}
+      {actionData && actionData.c10Rating && <p>{actionData.c10Rating}</p>}
+    </Fragment>
   );
 }
 
