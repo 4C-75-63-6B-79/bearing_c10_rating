aece495604443295d61e55fe8fd92974b1bf35d3
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2024-01-18 21:51:04 +0530

    created a result component to show the result of the things when successful calculation is done

---
+++ src/Components/Result/Result.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+
+function Result({ resultData }) {
+  return (
+    <div className="result">
+      <p>
+        <b>{resultData["parameterName"]}</b>
+        {resultData["alternativeParameterName"] && " or "}
+        {resultData["alternativeParameterName"] && (
+          <b>{resultData["alternativeParameterName"]}</b>
+        )}
+        {resultData["parameterSymbol"] && " ("}
+        {resultData["parameterSymbol"] && (
+          <i>{resultData["parameterSymbol"]}</i>
+        )}
+        {resultData["parameterSubSymbol"] && (
+          <i>
+            <sub>{resultData["parameterSubSymbol"]}</sub>
+          </i>
+        )}
+        {resultData["parameterSymbol"] && ")"} is{" "}
+        <b>
+          <i>{resultData["value"]}</i>
+        </b>
+        {resultData["unit"] && " "}
+        {resultData["unit"] && (
+          <b>
+            <i>{resultData["unit"]}</i>
+          </b>
+        )}
+      </p>
+    </div>
+  );
+}
+
+export default Result;
+
+Result.propTypes = {
+  resultData: PropTypes.shape({
+    parameterName: PropTypes.string.isRequired,
+    alternativeParameterName: PropTypes.string.isRequired,
+    parameterSymbol: PropTypes.string.isRequired,
+    parameterSubSymbol: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    unit: PropTypes.string.isRequired,
+  }),
+};
