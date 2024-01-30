d1191126c002ae4369698a5521861889b9ee3d7b
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2024-01-30 23:23:37 +0530

    created the component for the text of relating load life reliability

---
+++ src/Components/TextRelatingLoadLifeReliability/TextRelatingLoadLifeReliability.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+function TextRelatingLoadLifeReliability() {
+  return (
+    <Fragment>
+      <h1>Text Relating Load, Life and Reliability</h1>
+    </Fragment>
+  );
+}
+
+export default TextRelatingLoadLifeReliability;
